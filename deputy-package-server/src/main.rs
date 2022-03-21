042c61a3d1b5fdbaa3457f1fac4549b1490be797
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-03-21 16:58:58 +0200

    add basic endpoints for the server

--- deputy-package-server/src/main.rs
+++ deputy-package-server/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web::scope, App, HttpServer};
+use deputy_package_server_routes::basic::{status, version};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(scope("/").service(status).service(version)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }

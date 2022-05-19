917bf0b80008412f7c0f688725fca90bd26562fc
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-05-19 13:51:56 +0300

    add basic error handling to deputy CLI

--- deputy/src/client.rs
+++ deputy/src/client.rs
@@ -1,8 +1,9 @@
 use crate::constants::endpoints::{LARGE_PACKAGE_UPLOAD_PATH, SMALL_PACKAGE_UPLOAD_PATH};
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Error, Result};
 use awc::Client as ActixWebClient;
 use deputy_library::package::PackageStream;
 use log::error;
+use std::str::from_utf8;
 
 pub struct Client {
     client: ActixWebClient,
@@ -17,9 +18,15 @@ impl Client {
         }
     }
 
+    fn response_to_error(message: &str, payload: Vec<u8>) -> Result<Error> {
+        let error_message = format!("{message}: {}", from_utf8(&payload)?);
+        error!("{error_message}");
+        Ok(anyhow!(error_message))
+    }
+
     pub async fn stream_large_package(&self, stream: PackageStream) -> Result<()> {
         let put_uri = format!("{}{}", self.api_base_url, LARGE_PACKAGE_UPLOAD_PATH);
-        let response = self
+        let mut response = self
             .client
             .put(put_uri)
             .send_stream(stream)
@@ -29,13 +36,15 @@ impl Client {
             return Ok(());
         }
 
-        error!("Failed to upload package");
-        Err(anyhow!("Failed to upload package"))
+        Err(Client::response_to_error(
+            "Failed to upload package",
+            response.body().await?.to_vec(),
+        )?)
     }
 
     pub async fn upload_small_package(&self, payload: Vec<u8>) -> Result<()> {
         let put_uri = format!("{}{}", self.api_base_url, SMALL_PACKAGE_UPLOAD_PATH);
-        let response = self
+        let mut response = self
             .client
             .put(put_uri)
             .send_body(payload)
@@ -45,7 +54,9 @@ impl Client {
             return Ok(());
         }
 
-        error!("Failed to upload package");
-        Err(anyhow!("Failed to upload package"))
+        Err(Client::response_to_error(
+            "Failed to upload package",
+            response.body().await?.to_vec(),
+        )?)
     }
 }
--- deputy/src/main.rs
+++ deputy/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
+use colored::Colorize;
 use deputy::configuration::Configuration;
 use deputy::executor::Executor;
 
@@ -21,10 +22,13 @@ async fn main() -> Result<()> {
     let args = Cli::parse();
     let executor = Executor::try_new(Configuration::get_configuration()?)?;
 
-    match args.command {
-        Commands::Publish => {
-            executor.publish().await?;
-        }
+    let result = match args.command {
+        Commands::Publish => executor.publish().await,
     };
+    if let Err(error) = result {
+        eprintln!("{} {}", "Error:".red(), error);
+        std::process::exit(1);
+    }
+
     Ok(())
 }

2bc57488cf97bfadc470f3206c4d829694af4530
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-05-27 17:31:23 +0300

    decrease small package size

--- deputy/src/constants.rs
+++ deputy/src/constants.rs
@@ -3,7 +3,7 @@ use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref SMALL_PACKAGE_LIMIT: u64 =
-        u64::try_from(Byte::from_str("10MB").unwrap().get_bytes()).unwrap();
+        u64::try_from(Byte::from_str("256kB").unwrap().get_bytes()).unwrap();
 }
 
 pub const CONFIG_FILE_PATH_ENV_KEY: &str = "DEPUTY_CONFIG";

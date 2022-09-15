1fb9badb5ae566c2f74a2b838d5672d17fc977b6
Madis-Mikk Remmet <madismikk.remmet@cr14.ee>
2022-09-15 06:10:21 +0000

    Add enters between related constants

--- deputy-library/src/constants.rs
+++ deputy-library/src/constants.rs
@@ -3,6 +3,7 @@ use lazy_static::lazy_static;
 use parse_duration::parse;
 use std::time::Duration;
 
+pub const LOCKFILE: &str = "deputy.lock";
 pub const LOCKFILE_TIMEOUT: &str = "5 minutes";
 pub const LOCKFILE_SLEEP: &str = "250 milliseconds";
 
@@ -17,9 +18,12 @@ lazy_static! {
 }
 
 pub const SHA256_LENGTH: usize = 64;
+
 pub const COMPRESSION_CHUNK_SIZE: usize = 131_072;
+
 pub const PAYLOAD_CHUNK_SIZE: u64 = 8192;
+
 pub const INDEX_REPOSITORY_BRANCH: &str = "master";
 pub const INDEX_REPOSITORY_REMOTE: &str = "origin";
+
 pub const CONFIGURATION_FOLDER_PATH_ENV_KEY: &str = "DEPUTY_CONFIG_FOLDER";
-pub const LOCKFILE: &str = "deputy.lock";

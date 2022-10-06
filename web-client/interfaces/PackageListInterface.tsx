c46f53aad2ab2acba2bb813801cf68ee0dbe8574
Robert Altmäe <robert.altmae@cr14.ee>
2022-10-06 13:44:03 +0300

    Add Account and Feature to frontend interface

--- web-client/interfaces/PackageListInterface.tsx
+++ web-client/interfaces/PackageListInterface.tsx
@@ -9,7 +9,14 @@ interface Content {
   type: string;
 }
 
+interface Account {
+  name: string;
+  password: string;
+}
+
 interface VirtualMachine {
+  accounts: Account[];
+  default_account: string;
   operating_system: string;
   architecture?: any;
   type: string;
@@ -17,8 +24,19 @@ interface VirtualMachine {
   readme_path?: any;
 }
 
+enum FeatureType {
+  SERVICE = 'SERVICE',
+  CONFIGURATION = 'CONFIGURATION',
+  ARTIFACT = 'ARTIFACT',
+}
+
+interface Feature {
+  feature_type: FeatureType;
+}
+
 export interface PackageList {
   package: Package;
   content: Content;
   virtual_machine: VirtualMachine;
+  feature: Feature;
 }

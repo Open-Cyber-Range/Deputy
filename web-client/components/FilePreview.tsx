52f2a856d15c6aac20c17f30d2d61d099a0814ba
Robert Altmäe <robert.altmae@cr14.ee>
2023-03-17 14:24:10 +0200

    Refactor styling

--- web-client/components/FilePreview.tsx
+++ web-client/components/FilePreview.tsx
@@ -6,9 +6,9 @@ import Lightbox from 'yet-another-react-lightbox';
 import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
 import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
 import Inline from 'yet-another-react-lightbox/plugins/inline';
+import Video from 'yet-another-react-lightbox/plugins/video';
 import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
-import Video from 'yet-another-react-lightbox/plugins/video';
 
 const FilePreview = ({packageData}: {packageData: Package}) => {
   const {asPath} = useRouter();
@@ -24,7 +24,7 @@ const FilePreview = ({packageData}: {packageData: Package}) => {
       if (preview.type === PreviewType.Picture) {
         preview.value.forEach(filepath => {
           slides.push({
-            height: 3300, width: 3300,
+            height: 10000, width: 10000,
             src: '/api/v1/package/' + nameAndVersion + '/path/' + filepath,
           });
         });
@@ -32,7 +32,7 @@ const FilePreview = ({packageData}: {packageData: Package}) => {
 
       if (preview.type === PreviewType.Video) {
         preview.value.forEach(filepath => {
-          slides.push({height: 3300, width: 3300, type: 'video', sources: [{
+          slides.push({height: 10000, width: 10000, type: 'video', sources: [{
             src: '/api/v1/package/' + nameAndVersion + '/path/' + filepath,
             type: 'video/mp4',
           }],
@@ -46,7 +46,7 @@ const FilePreview = ({packageData}: {packageData: Package}) => {
   return (
     <Lightbox
       slides={slides}
-      inline={{style: {width: '100%', maxWidth: '900px', aspectRatio: '3 / 2'}}}
+      inline={{style: {aspectRatio: '3 / 2'}}}
       plugins={[Video, Thumbnails, Inline, Fullscreen]}
     />
   );

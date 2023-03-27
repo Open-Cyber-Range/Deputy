768715f508863ba69aeea1f927a89cfd7147b447
Robert Altmäe <robert.altmae@cr14.ee>
2023-03-27 15:46:43 +0300

    Add nextauthjs library for Keycloak integration, WIP

--- web-client/pages/_app.tsx
+++ web-client/pages/_app.tsx
@@ -3,10 +3,12 @@ import '@blueprintjs/core/lib/css/blueprint.css';
 import '../styles/global.css';
 import Head from 'next/head';
 import useTranslation from 'next-translate/useTranslation';
+import { SessionProvider } from "next-auth/react";
 import MainNavbar from '../components/MainNavbar';
 
-const Deputy = ({ Component, pageProps }: AppProps) => {
-  const { t } = useTranslation('common');
+
+function MyApp({Component, pageProps: { session, ...pageProps}}: AppProps) {
+  const {t} = useTranslation('common');
 
   return (
     <>
@@ -14,13 +16,10 @@ const Deputy = ({ Component, pageProps }: AppProps) => {
         <title>{t('title')}</title>
         <meta name={t('metaName')} content={t('metaContent')} />
       </Head>
-      <div className="flex flex-col min-h-screen">
-        <MainNavbar />
-        <main className="grow">
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <MainNavbar />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 };
--- web-client/pages/index.tsx
+++ web-client/pages/index.tsx
@@ -3,9 +3,11 @@ import { Button, H3 } from '@blueprintjs/core';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import styles from '../styles/Index.module.css';
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Home: NextPage = () => {
-  const { t } = useTranslation('common');
+  const {t} = useTranslation('common');
+  const { data: session } = useSession()
   const router = useRouter();
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
@@ -14,17 +16,31 @@ const Home: NextPage = () => {
       router.push(process.env.DOCUMENTATION_URL);
     }
   };
+  if (session) {
 
-  return (
-    <div>
-      <div className={styles.dashboard}>
-        <H3>{t('welcome')}</H3>
-        <Button intent="primary" large onClick={handleClick}>
-          {t('documentationButton')}
-        </Button>
+    console.log(session.user?.email);
+    return (
+      <div>
+        <main className={styles.main}>
+          <div className={styles.dashboard}>
+            <h1>{t('welcome')}</h1>
+            <Button intent='primary' large onClick={handleClick}>{t('documentationButton')}</Button>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        </main>
+
+        <footer className={styles.footer}>
+          <span>{t('footer')}</span>
+        </footer>
       </div>
-    </div>
-  );
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 };
 
 export default Home;

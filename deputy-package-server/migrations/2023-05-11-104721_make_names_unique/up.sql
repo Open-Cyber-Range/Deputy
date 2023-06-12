adfe22f20fa900aa5ee483d29d0664c1f76c315f
Robert Altmäe <robert.altmae@cr14.ee>
2023-06-12 15:05:06 +0300

    Fix name uniqueness migration

--- deputy-package-server/migrations/2023-05-11-104721_make_names_unique/up.sql
+++ deputy-package-server/migrations/2023-05-11-104721_make_names_unique/up.sql
@@ -1,2 +1,21 @@
+ALTER TABLE versions
+    DROP FOREIGN KEY FK_PackageVersion;
+
+CREATE TABLE unique_names AS
+SELECT MIN(id) AS id, name
+FROM packages
+GROUP BY name;
+
+TRUNCATE TABLE packages;
+
 ALTER TABLE packages
     ADD CONSTRAINT UC_UniqueName UNIQUE (name);
+
+INSERT INTO packages (id, name)
+SELECT id, name
+FROM unique_names;
+
+DROP TABLE unique_names;
+
+ALTER TABLE versions
+    ADD CONSTRAINT FK_PackageVersion FOREIGN KEY (package_id) REFERENCES packages(id);

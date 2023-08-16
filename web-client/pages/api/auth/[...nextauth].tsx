78cae62174c891ce09cc92ed4dcb7a0b6492de34
Kaarel Allemann <kaarel.allemann@cr14.ee>
2023-08-16 10:20:19 +0000

    add id token to auth session

--- web-client/pages/api/auth/[...nextauth].tsx
+++ web-client/pages/api/auth/[...nextauth].tsx
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from 'next-auth';
+import NextAuth, { AuthOptions, Session } from 'next-auth';
 import KeycloakProvider from 'next-auth/providers/keycloak';
 
 export const authOptions: AuthOptions = {
@@ -9,6 +9,29 @@ export const authOptions: AuthOptions = {
       issuer: process.env.KEYCLOAK_ISSUER,
     }),
   ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        // eslint-disable-next-line no-param-reassign
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      const sessionParamter: Session & { idToken?: string } = {
+        ...session,
+      };
+      if (token.idToken) {
+        // eslint-disable-next-line no-param-reassign
+        sessionParamter.idToken = token.idToken as string;
+      }
+      return sessionParamter;
+    },
+  },
+  debug: true,
 };
 
 export default NextAuth(authOptions);

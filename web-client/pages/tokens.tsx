1db6ac1192d3cf145ed4f2fddc75f9eae81f4be6
Kaarel Allemann <kaarel.allemann@cr14.ee>
2023-08-18 12:43:10 +0000

    improved

--- web-client/pages/tokens.tsx
+++ web-client/pages/tokens.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import {
   Button,
+  Callout,
   Card,
   Dialog,
   DialogBody,
@@ -10,6 +11,9 @@ import {
   H5,
   HTMLTable,
   InputGroup,
+  OverlayToaster,
+  Position,
+  Toast,
 } from '@blueprintjs/core';
 import useTranslation from 'next-translate/useTranslation';
 import { useState } from 'react';
@@ -22,18 +26,20 @@ const Tokens: NextPage = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [tokenName, setTokenName] = useState('');
   const [createdTokens, setCreatedTokens] = useState<Token[]>([]);
+  const [creationError, setCreationError] = useState<string | null>(null);
 
   const { data: fetchedTokens, error } = useSWR(
     '/api/v1/token',
     apiTokenFetcher
   );
 
-  if (error || !fetchedTokens) {
-    return <div>{t('failedToFetchTokens')} </div>;
-  }
-
   return (
     <div>
+      <OverlayToaster position={Position.TOP}>
+        {creationError && (
+          <Toast timeout={10} intent="danger" message={creationError} />
+        )}
+      </OverlayToaster>
       <main className="flex flex-row justify-center">
         <div className="flex flex-col items-strech w-full max-w-2xl">
           <div className="flex flex-row mt-6 items-end justify-between">
@@ -71,64 +77,82 @@ const Tokens: NextPage = () => {
                     intent="primary"
                     text={t('create')}
                     onClick={async () => {
-                      const newToken = await createToken({
-                        name: tokenName,
-                      });
-                      setCreatedTokens([...createdTokens, newToken]);
-                      setIsDialogOpen(false);
+                      try {
+                        const newToken = await createToken({
+                          name: tokenName,
+                        });
+                        setCreatedTokens([...createdTokens, newToken]);
+                        setIsDialogOpen(false);
+                      } catch (err) {
+                        setCreationError(t('failedToCreateToken'));
+                      }
+                      setTokenName('');
                     }}
                   />
                 }
               />
             </Dialog>
           </div>
-          {createdTokens
-            .sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt))
-            .map((token) => (
-              <Card
-                className="mt-10"
-                interactive={false}
-                elevation={Elevation.TWO}
-              >
-                <H5>{token.name}</H5>
-
-                <Button
-                  icon="clipboard"
-                  minimal
-                  onClick={() => {
-                    navigator.clipboard.writeText(token.token);
-                  }}
-                >
-                  {t('copyValue')}
-                </Button>
-              </Card>
-            ))}
-          <HTMLTable striped bordered className="mt-10">
-            <thead>
-              <tr>
-                <th>{t('name')}</th>
-                <th>{t('createdAt')}</th>
-                <th> </th>
-              </tr>
-            </thead>
-            <tbody>
-              {fetchedTokens
+          <div className="flex flex-row justify-center">
+            <div className="flex flex-col w-full max-w-sm">
+              {createdTokens
                 .sort(
                   (a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)
                 )
                 .map((token) => (
-                  <tr key={token.id}>
-                    <td>{token.name}</td>
-                    <td>{new Date(token.createdAt).toLocaleString()}</td>
-                    <td>
-                      <Button intent="danger" icon="trash" small>
-                        {t('delete')}
-                      </Button>
-                    </td>
-                  </tr>
+                  <Card
+                    className="mt-10 flex flex-row justify-between items-center"
+                    interactive={false}
+                    elevation={Elevation.TWO}
+                  >
+                    <H5 className="m-0 text-ellipsis">{token.name}</H5>
+
+                    <Button
+                      icon="clipboard"
+                      minimal
+                      onClick={() => {
+                        navigator.clipboard.writeText(token.token);
+                      }}
+                    >
+                      {t('copyValue')}
+                    </Button>
+                  </Card>
                 ))}
-            </tbody>
-          </HTMLTable>
+            </div>
+          </div>
+          <div className="mt-10">
+            {error ? (
+              <Callout intent="danger" title={t('failedToFetchTokens')} />
+            ) : (
+              <HTMLTable striped bordered>
+                <thead>
+                  <tr>
+                    <th>{t('name')}</th>
+                    <th>{t('createdAt')}</th>
+                    <th> </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {(fetchedTokens ?? [])
+                    .sort(
+                      (a, b) =>
+                        Date.parse(b.createdAt) - Date.parse(a.createdAt)
+                    )
+                    .map((token) => (
+                      <tr key={token.id}>
+                        <td className="text-ellipsis">{token.name}</td>
+                        <td>{new Date(token.createdAt).toLocaleString()}</td>
+                        <td className="flex justify-end">
+                          <Button intent="danger" icon="trash" small>
+                            {t('delete')}
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </HTMLTable>
+            )}
+          </div>
         </div>
       </main>
     </div>

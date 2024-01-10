5875830750d5ffecfb902ea77976169fb5144610
Kristi Reispass <kristi.reispass@cr14.ee>
2024-01-10 20:01:33 +0000

    add empty search handling

--- web-client/components/SearchBar.tsx
+++ web-client/components/SearchBar.tsx
@@ -1,13 +1,20 @@
 import useTranslation from 'next-translate/useTranslation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Icon, InputGroup } from '@blueprintjs/core';
+import {
+  Icon,
+  InputGroup,
+  OverlayToaster,
+  Position,
+  Toast,
+} from '@blueprintjs/core';
 import { extractAndRemoveTypeAndCategory } from '../utils';
 
 const SearchBar = () => {
   const { t } = useTranslation('common');
   const [searchInput, setSearchInput] = useState('');
   const router = useRouter();
+  const [isEmptySearch, setIsEmptySearch] = useState(false);
 
   const parsedSearchInput = extractAndRemoveTypeAndCategory(searchInput);
 
@@ -29,22 +36,52 @@ const SearchBar = () => {
 
   const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (!searchInput.trim()) {
+      setIsEmptySearch(true);
+      return;
+    }
+    setIsEmptySearch(false);
     router.push(`${searchUrl}`);
   };
 
   const handleBlur = () => {
     setSearchInput('');
+    setIsEmptySearch(false);
   };
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isEmptySearch) {
+      timer = setTimeout(() => setIsEmptySearch(false), 5000);
+    }
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [isEmptySearch]);
 
   return (
     <form onSubmit={handleSearchSubmit}>
+      <OverlayToaster position={Position.TOP}>
+        {isEmptySearch && (
+          <Toast
+            icon="warning-sign"
+            intent="warning"
+            message={t('emptySearchInput')}
+          />
+        )}
+      </OverlayToaster>
       <InputGroup
         className="m-[1rem]"
         leftIcon={<Icon icon="search" />}
         type="search"
         placeholder={t('searchbox')}
         value={searchInput}
-        onChange={(event) => setSearchInput(event.target.value)}
+        onChange={(event) => {
+          setSearchInput(event.target.value);
+          setIsEmptySearch(false);
+        }}
         onBlur={handleBlur}
       />
     </form>
